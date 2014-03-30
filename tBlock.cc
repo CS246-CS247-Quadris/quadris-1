b64d1428afd139efa575488779bca560ae5914a9
Sebastian <sebastian@ubuntu.ubuntu-domain>
2014-03-30 19:07:40 -0400

    all rotations done

--- tBlock.cc
+++ tBlock.cc
@@ -11,50 +11,46 @@ tBlock::~tBlock() {}
 
 void tBlock::rotateCCW() {
 
-    if(cells.at(0)->x > cells.at(3)->x) {
-	cells.at(0)->setXY(cells.at(0)->x,cells.at(0)->y+1);
-	cells.at(1)->setXY(cells.at(1)->x-1,cells.at(1)->y);
-	cells.at(2)->setXY(cells.at(2)->x-2,cells.at(2)->y-1);
-	cells.at(3)->setXY(cells.at(3)->x+1,cells.at(3)->y);
-  } else if(cells.at(0)->y > cells.at(3)->y){
-	cells.at(0)->setXY(cells.at(0)->x-1,cells.at(0)->y+1);
-	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y);
-	cells.at(2)->setXY(cells.at(2)->x+1,cells.at(2)->y-1);
-	cells.at(3)->setXY(cells.at(3)->x,cells.at(3)->y+2);
-  }else if(cells.at(0)->x < cells.at(3)->x){
-	cells.at(0)->setXY(cells.at(0)->x-1,cells.at(0)->y-2);
+    if(cells.at(1)->x < cells.at(3)->x) {
+	cells.at(0)->setXY(cells.at(0)->x+1,cells.at(0)->y);
 	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y-1);
-	cells.at(2)->setXY(cells.at(2)->x+1,cells.at(2)->y);
-	cells.at(3)->setXY(cells.at(3)->x-2,cells.at(3)->y-1);
-  }else if(cells.at(0)->y < cells.at(3)->y){
-	cells.at(0)->setXY(cells.at(0)->x+2,cells.at(0)->y);
+	cells.at(2)->setXY(cells.at(2)->x-1,cells.at(2)->y-2);
+	cells.at(3)->setXY(cells.at(3)->x-1,cells.at(3)->y);
+  } else if(cells.at(1)->y < cells.at(3)->y){
+	cells.at(0)->setXY(cells.at(0)->x,cells.at(0)->y+2);
 	cells.at(1)->setXY(cells.at(1)->x+1,cells.at(1)->y+1);
-	cells.at(2)->setXY(cells.at(2)->x,cells.at(2)->y+2);
-	cells.at(3)->setXY(cells.at(3)->x+1,cells.at(3)->y-1);
+	cells.at(2)->setXY(cells.at(2)->x+2,cells.at(2)->y);
+	cells.at(3)->setXY(cells.at(3)->x,cells.at(3)->y);
+  }else if(cells.at(1)->x > cells.at(3)->x){
+	cells.at(0)->setXY(cells.at(0)->x-2,cells.at(0)->y-1);
+	cells.at(1)->setXY(cells.at(1)->x-1,cells.at(1)->y);
+	cells.at(2)->setXY(cells.at(2)->x,cells.at(2)->y+1);
+	cells.at(3)->setXY(cells.at(3)->x,cells.at(3)->y-1);
+  }else if(cells.at(1)->y > cells.at(3)->y){
+	cells.at(0)->setXY(cells.at(0)->x+1,cells.at(0)->y-1);
+	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y);
+	cells.at(2)->setXY(cells.at(2)->x-1,cells.at(2)->y+1);
+	cells.at(3)->setXY(cells.at(3)->x+1,cells.at(3)->y+1);
   }
 }
 
 void tBlock::rotateCW() {
     if(cells.at(1)->x < cells.at(3)->x) {
-	std::cout<<"here1"<<std::endl;
 	cells.at(0)->setXY(cells.at(0)->x-1,cells.at(0)->y+1);
 	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y);
 	cells.at(2)->setXY(cells.at(2)->x+1,cells.at(2)->y-1);
 	cells.at(3)->setXY(cells.at(3)->x-1,cells.at(3)->y-1);
   } else if(cells.at(1)->y > cells.at(3)->y){
-	std::cout<<"here2"<<std::endl;
 	cells.at(0)->setXY(cells.at(0)->x+2,cells.at(0)->y+1);
 	cells.at(1)->setXY(cells.at(1)->x+1,cells.at(1)->y);
 	cells.at(2)->setXY(cells.at(2)->x,cells.at(2)->y-1);
 	cells.at(3)->setXY(cells.at(3)->x,cells.at(3)->y+1);
   }else if(cells.at(1)->x > cells.at(3)->x){
-	std::cout<<"here3"<<std::endl;
 	cells.at(0)->setXY(cells.at(0)->x,cells.at(0)->y-2);
 	cells.at(1)->setXY(cells.at(1)->x-1,cells.at(1)->y-1);
 	cells.at(2)->setXY(cells.at(2)->x-2,cells.at(2)->y);
 	cells.at(3)->setXY(cells.at(3)->x,cells.at(3)->y);
   }else if(cells.at(1)->y < cells.at(3)->y){
-	std::cout<<"here4"<<std::endl;
 	cells.at(0)->setXY(cells.at(0)->x-1,cells.at(0)->y);
 	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y+1);
 	cells.at(2)->setXY(cells.at(2)->x+1,cells.at(2)->y+2);
