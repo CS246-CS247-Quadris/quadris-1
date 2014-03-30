428f9316e1ec2abdb257a48bda3436b951a9fab6
Sebastian <sebastian@ubuntu.ubuntu-domain>
2014-03-30 18:29:13 -0400

    Iblock

--- iBlock.cc
+++ iBlock.cc
@@ -4,10 +4,10 @@
 using namespace std;
 
 iBlock::iBlock(gameBoard * pBoard):Block(pBoard) {
-	cells.push_back(new Cell(3,0,'i',current_level,true));
-	cells.push_back(new Cell(3,1,'i',current_level,true));
-	cells.push_back(new Cell(3,2,'i',current_level,true));
-	cells.push_back(new Cell(3,3,'i',current_level,true));
+	cells.push_back(new Cell(3,0,'I',current_level,true));
+	cells.push_back(new Cell(3,1,'I',current_level,true));
+	cells.push_back(new Cell(3,2,'I',current_level,true));
+	cells.push_back(new Cell(3,3,'I',current_level,true));
 	block_type = 'i';
 	current_block_id++;
 }
@@ -19,15 +19,18 @@ void iBlock::rotateCCW() {
 
 void iBlock::rotateCW() {
   if(cells.at(0)->x != cells.at(3)->x) {
-	cells.at(1)->setXY(cells.at(0)->x,cells.at(0)->y-1);
-	cells.at(2)->setXY(cells.at(0)->x,cells.at(0)->y-2);
-	cells.at(3)->setXY(cells.at(0)->x,cells.at(0)->y-3);
+	cells.at(0)->setXY(cells.at(0)->x,cells.at(0)->y);
+	cells.at(1)->setXY(cells.at(1)->x+1,cells.at(1)->y+1);
+	cells.at(2)->setXY(cells.at(2)->x+2,cells.at(2)->y+2);
+	cells.at(3)->setXY(cells.at(3)->x+3,cells.at(3)->y+3);
   } else {
-	cells.at(1)->setXY(cells.at(0)->x-1,cells.at(0)->y);
-	cells.at(2)->setXY(cells.at(0)->x-2,cells.at(0)->y);
-	cells.at(3)->setXY(cells.at(0)->x-3,cells.at(0)->y);
+	cells.at(0)->setXY(cells.at(0)->x,cells.at(0)->y);
+	cells.at(1)->setXY(cells.at(1)->x-1,cells.at(1)->y-1);
+	cells.at(2)->setXY(cells.at(2)->x-2,cells.at(2)->y-2);
+	cells.at(3)->setXY(cells.at(3)->x-3,cells.at(3)->y-3);
   }
 }
+
 
 void iBlock::left(){
 	cells.at(0)->setXY(cells.at(0)->x,cells.at(0)->y-1);
