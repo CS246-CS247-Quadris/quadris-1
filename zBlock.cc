1ee74595ecde7cedaa38c44cd1b01c37650fd338
Sebastian <sebastian@ubuntu.ubuntu-domain>
2014-03-31 17:37:22 -0400

    sblock zblock

--- zBlock.cc
+++ zBlock.cc
@@ -1,5 +1,5 @@
 #include "zBlock.h"
-
+using namespace std;
 zBlock::zBlock(gameBoard * pBoard):Block(pBoard) {
 	cells.push_back(new Cell(1,0,'Z',current_level,true));
 	cells.push_back(new Cell(1,1,'Z',current_level,true));
@@ -16,16 +16,27 @@ void zBlock::rotateCCW() {
 
 void zBlock::rotateCW() {
     if(cells.at(2)->x == cells.at(3)->x) {
+if(isLegalXY(cells.at(0)->x+1,cells.at(0)->y) && isLegalXY(cells.at(1)->x,cells.at(1)->y-1) && isLegalXY(cells.at(2)->x-1,cells.at(2)->y) && isLegalXY(cells.at(3)->x-2,cells.at(3)->y-1)){
+	cout << "it is legal" << endl;
 	cells.at(0)->setXY(cells.at(0)->x+1,cells.at(0)->y);
 	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y-1);
 	cells.at(2)->setXY(cells.at(2)->x-1,cells.at(2)->y);
 	cells.at(3)->setXY(cells.at(3)->x-2,cells.at(3)->y-1);
+	}else{
+		cout << "it is not legal" << endl;
+	}
   } else{
+if(isLegalXY(cells.at(0)->x-1,cells.at(0)->y) && isLegalXY(cells.at(1)->x,cells.at(1)->y+1) && isLegalXY(cells.at(2)->x+1,cells.at(2)->y) && isLegalXY(cells.at(3)->x+2,cells.at(3)->y+1)){
+	cout << "it is legal" << endl;
 	cells.at(0)->setXY(cells.at(0)->x-1,cells.at(0)->y);
 	cells.at(1)->setXY(cells.at(1)->x,cells.at(1)->y+1);
 	cells.at(2)->setXY(cells.at(2)->x+1,cells.at(2)->y);
 	cells.at(3)->setXY(cells.at(3)->x+2,cells.at(3)->y+1);
+	}else{
+		cout << "it is not legal" << endl;
+	}
   }
+
 }
 
 void zBlock::left(){
